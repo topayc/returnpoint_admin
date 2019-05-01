0ec3d94aefecb81bb00eaa38e3b1bcccabd07c81
topayc <topayc@naver.com>
2019-05-01 23:59:09 +0900

    .....

--- src/main/java/com/returnp/admin/dto/command/AffiliateTidCommand.java
+++ src/main/java/com/returnp/admin/dto/command/AffiliateTidCommand.java
@@ -4,6 +4,25 @@ import com.returnp.admin.model.AffiliateTid;
 
 public class AffiliateTidCommand extends AffiliateTid {
 	private String affiliateName;
+	private String affiliateEmail;;
+	private Integer memberNo;;
+
+	
+	public String getAffiliateEmail() {
+		return affiliateEmail;
+	}
+
+	public void setAffiliateEmail(String affiliateEmail) {
+		this.affiliateEmail = affiliateEmail;
+	}
+
+	public Integer getMemberNo() {
+		return memberNo;
+	}
+
+	public void setMemberNo(Integer memberNo) {
+		this.memberNo = memberNo;
+	}
 
 	public String getAffiliateName() {
 		return affiliateName;
